ff6be9595ebf4759ab5f8f08f1fcb79f46b59150
Mohamed Salama Shakir <salamam301@yahoo.com>
2021-02-09 19:49:17 +0200

    Adding Question CRUD views
    Adding Question CRUD views

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -12,5 +12,37 @@ class QuestionController extends Controller
         return view('questions.index',compact('questions'));
     }
 
+    public function create(){
+        return view('questions.create');
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id){
+        $question = Question::find($id);
+        return view('questions.show',compact('question'));
+    }
+
+    public function edit($id){
+        $question = Question::find($id);
+        return view('questions.edit',compact('question'));
+    }
+
+    public function update(Request $request,$id){
+
+    }
+
+    public function destroy($id){
+
+    }
+
+
+
+
+
+
+
 
 }
---
+++ resources/views/questions/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+     Question "{{$question->title}}"
+@endsection
