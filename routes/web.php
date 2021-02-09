f3a44bb7f834d9d5f93f01e19e4500885ee3a570
Mohamed Salama Shakir <salamam301@yahoo.com>
2021-02-09 16:27:33 +0200

    Creating Question Model, Migration and Controller
    Creating Question Model, Migration and Controller

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware(['auth'])->group(function (){
+//    Questions routes
+
+
+});
+
 require __DIR__.'/auth.php';
